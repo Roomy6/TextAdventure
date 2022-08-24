bbdf61d2908be5b112f6061ad57cbd3102f6f98a
Roomy6 <73183827+Roomy6@users.noreply.github.com>
2022-08-24 21:57:43 +0100

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,17 +1,23 @@
-# include <stdio.h>		// Defines scanf and printf
-# include <math.h>		// Has math functions
-# include <stdlib.h>	// Defines rand
-# include <string.h>	// Defines String
-# include <time.h>		// Defines time
+#include <stdio.h>		// Defines scanf and printf
+#include <math.h>		// Has math functions
+#include <stdlib.h>		// Defines rand
+#include <string.h>		// Defines String
+#include <time.h>		// Defines time
 
 
 int main(void)
 {
 	int PlayerMotion;
 
+	// Cant Figure out coin system
+	char Coins[] = "15";
+	char charCoins[] = "Coins: ", Coins[100];
+
 	char name[] = "";
 
-	printf("Welcome Player!\n");
+	printf("Dear Player,\n");
+	printf("This Game is purely for a simple project ;)\n\n");
+	printf(charCoins,"\n");
 
 	return 0;
 }
