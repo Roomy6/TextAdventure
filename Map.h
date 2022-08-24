51ed3acbad83b27835d3c9b36244eec711a351d1
Roomy6 <73183827+Roomy6@users.noreply.github.com>
2022-08-24 23:26:10 +0100

    Create Map.h

---
+++ Map.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdio.h>		// Defines scanf and printf
+#include <math.h>		// Has math functions
+#include <stdlib.h>		// Defines rand
+#include <string.h>		// Defines String
+#include <time.h>		// Defines time
+
+void Map(void)
+{
+	// aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+}
