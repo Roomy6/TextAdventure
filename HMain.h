54a58f8f7b9c7d97f89f1db1ec9acff728b449d8
Roomy6 <73183827+Roomy6@users.noreply.github.com>
2022-08-24 23:08:56 +0100

    .

--- HMain.h
+++ HMain.h
@@ -1,1 +1,61 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>		// Defines scanf and printf
+#include <math.h>		// Has math functions
+#include <stdlib.h>		// Defines rand
+#include <string.h>		// Defines String
+#include <time.h>		// Defines time
+
+
+
+void delay(int number_of_seconds)
+{
+	// Converting time into milli_seconds
+	int milli_seconds = 1000 * number_of_seconds;
+
+	// Storing start time
+	clock_t start_time = clock();
+
+	// looping till required time is not achieved
+	while (clock() < start_time + milli_seconds);
+}
+
+void player(void)
+{
+	char name[20];
+	int gender;
+
+	printf("|----------------------------------|");
+	printf("|								   |");
+	printf("Enter a player name: \n");
+	scanf("%s", name);
+	printf("Welcome %s!\n", name);
+
+}
+
+void stats(void)
+{
+	printf("stats\n");
+}
+
+void coins(void)
+{
+	int intCoins = 0;
+	char charCoins[] = "Coins: ";
+
+	// Cant Figure out coin system
+	// nvm i did :D
+	// https://stackoverflow.com/questions/347132/append-an-int-to-char
+
+	printf("%s %d\n", charCoins, intCoins);
+}
+
+void attacks(void)
+{
+	printf("attacks\n");
+}
+
+void story(void)
+{
+	printf("story\n");
+}
--- Main.c
+++ Main.c
@@ -1,5 +1,6 @@
 // https://github.com/Imtial/TextAdventure/blob/master/adventure.c
 // https://www.geeksforgeeks.org/time-delay-c/
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>		// Defines scanf and printf
 #include <math.h>		// Has math functions
@@ -7,54 +8,9 @@
 #include <string.h>		// Defines String
 #include <time.h>		// Defines time
 
+#include "HMain.h";
 
 
-void delay(int number_of_seconds)
-{
-	// Converting time into milli_seconds
-	int milli_seconds = 1000 * number_of_seconds;
-
-	// Storing start time
-	clock_t start_time = clock();
-
-	// looping till required time is not achieved
-	while (clock() < start_time + milli_seconds);
-}
-
-void player(void)
-{
-	char name[] = "";
-	char gender[] = "";
-
-	printf("Hello Player!");
-}
-
-void stats(void)
-{
-	printf("stats");
-}
-
-void coins(void)
-{
-	int intCoins = 0;
-	char charCoins[] = "Coins: ";
-
-	// Cant Figure out coin system
-	// nvm i did :D
-	// https://stackoverflow.com/questions/347132/append-an-int-to-char
-
-	printf("%s %d", charCoins, intCoins);
-}
-
-void attacks(void)
-{
-	printf("attacks");
-}
-
-void story(void)
-{
-	printf("story");
-}
 
 int main(void)
 {
@@ -62,14 +18,14 @@ int main(void)
 	printf("Dear Player,\n");
 	printf("This Game is purely a simple project ;)\n\n");
 
-	// Add's a delay of 5 seconds.
-	delay(5);
+	// Add's a delay of 2 seconds.
+	delay(2);
 
 	// Clears the console.
 	system("cls");
 
 	// forgot this();
-	story();
+	player();
 
 	return 0;
 }
